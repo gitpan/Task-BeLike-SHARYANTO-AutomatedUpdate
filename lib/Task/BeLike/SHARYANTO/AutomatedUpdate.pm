c33c99f0f6d6fbaba3ce41f6fb2e3125b614e4c2
Steven Haryanto <stevenharyanto@gmail.com>
2012-10-02 04:36:31 +0000

    Import of SHARYANTO/Task-BeLike-SHARYANTO-AutomatedUpdate-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-SHARYANTO-AutomatedUpdate
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-BeLike-SHARYANTO-AutomatedUpdate-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/SHARYANTO/AutomatedUpdate.pm
@@ -0,0 +1,55 @@
+package Task::BeLike::SHARYANTO::AutomatedUpdate;
+
+our $VERSION = '0.01'; # VERSION
+
+
+1;
+# ABSTRACT: SHARYANTO's modules that can be updated automatically by the author with a script
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::BeLike::SHARYANTO::AutomatedUpdate - SHARYANTO's modules that can be updated automatically by the author with a script
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+List of all SHARYANTO's modules that can be updated automatically by the author
+with a script.
+
+This includes all/most modules which use L<http://gudangdata.org> as source.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Business::BankCard::Indonesia>
+
+=head3 L<Business::BankList::Indonesia>
+
+=head3 L<Locale::ID::Locality>
+
+=head3 L<Locale::ID::Province>
+
+=head3 L<Task::BeLike::SHARYANTO::Authored>
+
+=head3 L<Task::BeLike::SHARYANTO::Favorited>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
